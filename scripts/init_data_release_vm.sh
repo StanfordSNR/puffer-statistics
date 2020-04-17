f5692356aa5b8f36cb28b8c2458fc40eb338a7ec
Emily Marx <emilykmarx@gmail.com>
2020-04-17 19:17:56 +0000

    small fix to init script

--- scripts/init_data_release_vm.sh
+++ scripts/init_data_release_vm.sh
@@ -4,14 +4,12 @@
 # Creates local data directory with watch times, in pwd.
 
 # 1. Clone puffer-statistics/data-release 
-
 git clone https://github.com/StanfordSNR/puffer-statistics.git 
 pushd puffer-statistics
 git checkout data-release   # TODO: this may change
 
-# 2. Install dependencies, set constants and paths used below
+# 2. Install dependencies
 scripts/deps.sh
-source scripts/export_constants.sh "$PWD"
 
 # 3. Build 
 ./autogen.sh
@@ -20,13 +18,14 @@ make -j$(nproc)
 sudo make install
 popd
 
-# 4. Create local directory for data
+# 4. Set constants and paths used below
+source scripts/export_constants.sh "$PWD"
+
+# 5. Create local directory for data
 mkdir "$LOCAL_DATA_PATH"
-pushd "$LOCAL_DATA_PATH"
+cd "$LOCAL_DATA_PATH"
 
-# 5. Download static watch time lists from gs (to be used for all days)
-gsutil cp -q "$DATA_BUCKET"/*"$WATCH_TIMES_POSTFIX" .
+# 6. Download static watch time lists from gs (to be used for all days)
+gsutil -q cp "$DATA_BUCKET"/*"$WATCH_TIMES_POSTFIX" .
 
-# 6. (Private only) Set up environment for Postgres connection 
-
-popd
+# 7. (Private only) Set up environment for Postgres connection 
