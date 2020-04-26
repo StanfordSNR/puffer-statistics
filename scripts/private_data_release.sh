114622fe612d723a89caa5978a22b1bb30785839
Puffer <puffer@puffer.stanford.edu>
2020-04-26 00:54:04 +0000

    push the correct private_data_release.sh

--- scripts/private_data_release.sh
+++ scripts/private_data_release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -ue 
 # Assumes puffer-statistics and local data live in ~
 cd ~/puffer-statistics/scripts
-source ./export_constants.sh ~ 2020-04-24 # TODO: test only!
-#source ./export_constants.sh ~ yesterday 
+source ./export_constants.sh ~ yesterday 
 echo "sourced"
 ./private_entrance.sh 
 echo "finished private"
