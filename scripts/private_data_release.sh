6b8c41e9731a66e4d6b91c1d8737eefbbe54fa93
Emily Marx <emilykmarx@gmail.com>
2020-05-11 22:57:20 +0000

    remove test version of private_data_release

--- scripts/private_data_release.sh
+++ scripts/private_data_release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ue 
 # Assumes puffer-statistics and local data live in ~
 cd ~/puffer-statistics/scripts
-source ./export_constants.sh ~ 2019-01-26
+source ./export_constants.sh ~ yesterday 
 echo "sourced"
 ./private_entrance.sh 
 echo "finished private"
 ./public_entrance.sh 
 echo "finished public"
-#./upload_public_results.sh 
+./upload_public_results.sh 
 echo "finished upload"
